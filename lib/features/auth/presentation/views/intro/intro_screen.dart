d911ccd698f2e3c0218c1ef71a1e6ae062fc1752
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2025-01-29 18:41:48 +0100

    added few screns

---
+++ lib/features/auth/presentation/views/intro/intro_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatefulWidget {
+  static const String routeName = '/intro';
+
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
