d6973848d9e7616ec7b84bbf5de5bdaac02f2da7
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2025-01-31 16:50:48 +0100

    update

--- lib/cores/utils/file_helper.dart
+++ lib/cores/utils/file_helper.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/foundation.dart';
-import 'package:image_picker/image_picker.dart';
 
 class FileHelper {
-  static final ImagePicker _picker = ImagePicker();
+  // static final ImagePicker _picker = ImagePicker();
 
-  static Future<String?> pickProfileImage() async {
-    final XFile? image = await _picker.pickImage(
-      source: ImageSource.gallery,
-      maxWidth: 400,
-      maxHeight: 400,
-      imageQuality: 70,
-    );
-    return image?.path;
-  }
-
-  static Future<String?> pickImage() async {
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-    return image?.path;
-  }
-
-  static Future<String?> takePicture() async {
-    // if (kDebugMode && Platform.isIOS) {
-    if (kDebugMode) {
-      return await pickImage();
-    }
-
-    final XFile? video = await _picker.pickImage(source: ImageSource.camera);
-    return video?.path;
-  }
+  // static Future<String?> pickProfileImage() async {
+  //   final XFile? image = await _picker.pickImage(
+  //     source: ImageSource.gallery,
+  //     maxWidth: 400,
+  //     maxHeight: 400,
+  //     imageQuality: 70,
+  //   );
+  //   return image?.path;
+  // }
+  //
+  // static Future<String?> pickImage() async {
+  //   final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+  //   return image?.path;
+  // }
+  //
+  // static Future<String?> takePicture() async {
+  //   // if (kDebugMode && Platform.isIOS) {
+  //   if (kDebugMode) {
+  //     return await pickImage();
+  //   }
+  //
+  //   final XFile? video = await _picker.pickImage(source: ImageSource.camera);
+  //   return video?.path;
+  // }
 }
